9ccba85b2c1fb0bda319c1da015f3b80b2b0d865
Artyom Manchenkov <artyom@manchenkoff.me>
2018-02-16 02:25:28 +0300

    minor update

--- migrations/m180214_210240_create_post_table.php
+++ migrations/m180214_210240_create_post_table.php
@@ -5,25 +5,42 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `post`.
  */
-class m180214_210240_create_post_table extends Migration
-{
+class m180214_210240_create_post_table extends Migration {
     /**
      * @inheritdoc
      */
-    public function up()
-    {
-        $this->createTable('post', [
+    public function up() {
+        $this->createTable('{{post}}', [
             'id' => $this->primaryKey(),
             'title' => $this->string(),
             'body' => $this->text(),
         ]);
+
+        $this->loadSampleData();
     }
 
     /**
      * @inheritdoc
      */
-    public function down()
-    {
-        $this->dropTable('post');
+    public function down() {
+        $this->dropTable('{{post}}');
+    }
+
+    public function loadSampleData() {
+        // First post
+        $post_item = new resty\models\Post();
+
+        $post_item->title = "First post";
+        $post_item->body = "Some post content information";
+
+        $post_item->save();
+
+        // Second post
+        $post_item = new resty\models\Post();
+
+        $post_item->title = "Second post";
+        $post_item->body = "Lorem ipsum is more interesting text";
+
+        $post_item->save();
     }
 }
